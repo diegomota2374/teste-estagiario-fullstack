a1808606d214d8b26e0dc6a369775db38fd10260
diegomota2374 <diegomota2374@gmail.com>
2024-08-29 16:22:24 -0300

    vercel

--- teste-estagiario-fullstack-backend/src/database/data-source.ts
+++ teste-estagiario-fullstack-backend/src/database/data-source.ts
@@ -7,11 +7,11 @@ const isProduction = process.env.NODE_ENV === "production";
 
 export const AppDataSource = new DataSource({
   type: "sqlite",
-  database: path.join(__dirname, "../database/database.sqlite"),
+  database: path.resolve(__dirname, "../database/database.sqlite"),
   synchronize: true,
   logging: true,
   entities: isProduction
-    ? [path.join(__dirname, "../entities/*.js")] // Use paths to compiled JavaScript files in production
+    ? [path.resolve(__dirname, "../entities/*.js")] // Use paths to compiled JavaScript files in production
     : [User, Task], // Use class references in development
   migrations: [],
   subscribers: [],
