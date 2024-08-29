7198e519a56f55a11660d02a2595f9a966e37dcc
diegomota2374 <diegomota2374@gmail.com>
2024-08-29 15:43:04 -0300

    vercel

--- teste-estagiario-fullstack-backend/src/database/data-source.ts
+++ teste-estagiario-fullstack-backend/src/database/data-source.ts
@@ -3,10 +3,10 @@ import path from "path";
 
 export const AppDataSource = new DataSource({
   type: "sqlite",
-  database: path.join(__dirname, "../database/database.sqlite"),
+  database: path.join(__dirname, "../src/database/database.sqlite"),
   synchronize: true,
   logging: false,
-  entities: [path.join(__dirname, "../entities/*.js")], // Adjust path for compiled JS files
+  entities: [path.join(__dirname, "../src/entities/*.js")], // Adjust path for compiled JS files
   migrations: [],
   subscribers: [],
 });
