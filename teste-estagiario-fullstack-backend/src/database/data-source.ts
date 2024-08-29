f3fbb1fb2480053a70e42c2515352ca824a2ccf6
diegomota2374 <diegomota2374@gmail.com>
2024-08-29 19:32:13 -0300

    vercel

--- teste-estagiario-fullstack-backend/src/database/data-source.ts
+++ teste-estagiario-fullstack-backend/src/database/data-source.ts
@@ -1,13 +1,19 @@
 import { DataSource } from "typeorm";
-import { User } from "../entities/User";
+import path from "path";
+import { User } from "../entities/User"; // Importar diretamente para desenvolvimento
 import { Task } from "../entities/Task";
+require("dotenv").config();
+
+const isProduction = process.env.NODE_ENV === "production";
 
 export const AppDataSource = new DataSource({
   type: "sqlite",
-  database: "./src/database/database.sqlite",
+  database: path.resolve(__dirname, "../database/database.sqlite"),
   synchronize: true,
-  logging: false,
-  entities: [User, Task],
+  logging: true,
+  entities: isProduction
+    ? [path.join(__dirname, "../entities/*.js")] // Certifique-se de que este caminho está correto
+    : [User, Task], // Utilizado em desenvolvimento
   migrations: [],
   subscribers: [],
 });
