247e8fc6c9f5d925f711e231a61d153fb7b136da
diegomota2374 <diegomota2374@gmail.com>
2024-08-30 10:25:58 -0300

    configue frontend for deploy

--- teste-estagiario-fullstack-backend/src/database/data-source.ts
+++ teste-estagiario-fullstack-backend/src/database/data-source.ts
@@ -1,18 +1,13 @@
 import { DataSource } from "typeorm";
-import path from "path";
-import { User } from "../entities/User"; // Certifique-se de que o caminho está correto
-import { Task } from "../entities/Task"; // Certifique-se de que o caminho está correto
-
-const isProduction = process.env.NODE_ENV === "production";
+import { User } from "../entities/User";
+import { Task } from "../entities/Task";
 
 export const AppDataSource = new DataSource({
   type: "sqlite",
-  database: path.resolve(__dirname, "../database/database.sqlite"),
+  database: "./database.sqlite",
   synchronize: true,
   logging: true,
-  entities: isProduction
-    ? [path.join(__dirname, "../entities/*.js")] // Usando caminho absoluto para arquivos JS em produção
-    : [User, Task], // Referências diretas às classes em desenvolvimento
+  entities: [User, Task],
   migrations: [],
   subscribers: [],
 });
